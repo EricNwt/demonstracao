923d5f78146c3d8e0ce607201d72bfc8f209aec3
skilobaloba@gmail.com <skilobaloba@gmail.com>
2023-05-03 13:12:12 -0300

    Criação do projeto

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function acucar(){
+    acucarmaisbarato.innerHTML = 'O açucar mais barato está no mercado Super Market no valor de 3,87R$'
+    acucarredeeconomia.innerHTML = 'Rede Economia não tem promoção'
+    acucarriosul.innerHTML = 'Rio Sul não tem promoção'
+    acucarsupermarket.innerHTML = 'Super Market Açucar refinado união: 3,87R$'
+    acucarsuperrede.innerHTML = 'Super Rede não tem promoção'
+}
+function cafe(){
+    cafemaisbarato.innerHTML = 'O café mais barato está no mercado Rede Economia no valor de 13,98R$'
+    caferedeeconomia.innerHTML = 'Rede Economia café tá bem extra forte: 13,98R$'
+    caferiosul.innerHTML = 'Rio Sul não tem promoção'
+    cafesupermarket.innerHTML = 'Super Market café melitta extra forte: 16,98R$'
+    cafesuperrede.innerHTML = 'Super Rede não tem promoção'
+}
+function frango(){
+    frangomaisbarato.innerHTML = 'O frango mais barato está no mercado Rede Economia no valor de 5,97R$'
+    frangoredeeconomia.innerHTML = 'Rede Economia Coxa com sobrecoxa de frango 1 quilo: 5,97R$ e Filezinho de peito de frango 1 quilo: 13,97R$'
+    frangoriosul.innerHTML = 'Rio Sul não tem promoção'
+    frangosupermarket.innerHTML = 'Super Market não tem promoção'
+    frangosuperrede.innerHTML = 'Super Rede não tem promoção'
+}
+function problema(){
+    alert("Favor entrar em contato pelo e-mail: skilobaloba@gmail.com")
+}
