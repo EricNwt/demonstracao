cf0b154b7cdfc5d65ca5a4682bb12d1d66ca6185
skilobaloba@gmail.com <skilobaloba@gmail.com>
2023-05-04 18:55:35 -0300

    Versão atualizada

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
 function acucar(){
     acucarmaisbarato.innerHTML = 'O açucar mais barato está no mercado Super Market no valor de 3,87R$'
     acucarredeeconomia.innerHTML = 'Rede Economia não tem promoção'
-    acucarriosul.innerHTML = 'Rio Sul não tem promoção'
+    acucarriosul.innerHTML = 'Rio Sul Açucar refinado caravelas: 3,99R$'
     acucarsupermarket.innerHTML = 'Super Market Açucar refinado união: 3,87R$'
     acucarsuperrede.innerHTML = 'Super Rede não tem promoção'
 }
+function arroz(){
+    arrozmaisbarato.innerHTML = 'O arroz mais barato está no mercado Rio Sul no valor de 19,99R$'
+    arrozredeeconomia.innerHTML = 'Rede Economia Arroz branco Palmares: 22,95R$'
+    arrozriosul.innerHTML = 'Rio Sul Arroz branco tucano: 19,99R$'
+    arrozsupermarket.innerHTML = 'Super Market não tem promoção'
+    arrozsuperrede.innerHTML = 'Super Rede não tem promoção'
+}
+function batatafrita(){
+    batatafritamaisbarato.innerHTML = 'A batata frita mais barata está no mercado Super Rede no valor de 6,99R$'
+    batatafritaredeeconomia.innerHTML = 'Rede Economia não tem promoção'
+    batatafritariosul.innerHTML = 'Rio Sul não tem promoção'
+    batatafritasupermarket.innerHTML = 'Super Market não tem promoção'
+    batatafritasuperrede.innerHTML = 'Super Rede batata frita quality fries: 6,99R$'
+}
 function cafe(){
-    cafemaisbarato.innerHTML = 'O café mais barato está no mercado Rede Economia no valor de 13,98R$'
-    caferedeeconomia.innerHTML = 'Rede Economia café tá bem extra forte: 13,98R$'
-    caferiosul.innerHTML = 'Rio Sul não tem promoção'
-    cafesupermarket.innerHTML = 'Super Market café melitta extra forte: 16,98R$'
-    cafesuperrede.innerHTML = 'Super Rede não tem promoção'
+    alert("Para ver adquira a versão completa")
+}
+function carne(){
+    alert("Para ver adquira a versão completa")
+}
+function cerveja(){
+    alert("Para ver adquira a versão completa")
+}
+function cremedeleite(){//Talvez possa ser retirado
+    alert("Para ver adquira a versão completa")
+}
+function detergente(){//Talvez possa ser retirado
+    alert("Para ver adquira a versão completa")
+}
+function feijao(){
+    alert("Para ver adquira a versão completa")
 }
 function frango(){
-    frangomaisbarato.innerHTML = 'O frango mais barato está no mercado Rede Economia no valor de 5,97R$'
-    frangoredeeconomia.innerHTML = 'Rede Economia Coxa com sobrecoxa de frango 1 quilo: 5,97R$ e Filezinho de peito de frango 1 quilo: 12,97R$'
-    frangoriosul.innerHTML = 'Rio Sul não tem promoção'
-    frangosupermarket.innerHTML = 'Super Market não tem promoção'
-    frangosuperrede.innerHTML = 'Super Rede não tem promoção'
+    alert("Para ver adquira a versão completa")
 }
-function compra(){
-    
+function leiteemcaixa(){
+    alert("Para ver adquira a versão completa")
+}
+function leiteempo(){//Talvez possa ser retirado
+    alert("Para ver adquira a versão completa")
+}
+function macarrao(){
+    alert("Para ver adquira a versão completa")
+}
+function margarina(){
+    alert("Para ver adquira a versão completa")
+}
+function molhodetomate(){//Talvez possa ser retirado
+    alert("Para ver adquira a versão completa")
+}
+function oleo(){
+    alert("Para ver adquira a versão completa")
+}
+function sabaoempo(){
+    alert("Para ver adquira a versão completa")
+}
+function rank(){
+    alert("Para ver adquira a versão completa")
 }
