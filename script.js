dd7ce2e9cd6ecf8773ee4161cf618312890c115e
skilobaloba@gmail.com <skilobaloba@gmail.com>
2023-05-05 16:34:22 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function acucar(){
-    acucarmaisbarato.innerHTML = 'O açucar mais barato está no mercado Super Market no valor de 3,87R$'
+    acucarmaisbarato.innerHTML = 'O açucar mais barato está no mercado Rio Sul no valor de 3,99R$'
     acucarredeeconomia.innerHTML = 'Rede Economia não tem promoção'
     acucarriosul.innerHTML = 'Rio Sul Açucar refinado caravelas: 3,99R$'
-    acucarsupermarket.innerHTML = 'Super Market Açucar refinado união: 3,87R$'
-    acucarsuperrede.innerHTML = 'Super Rede não tem promoção'
+    acucarsupermarket.innerHTML = 'Super Market não tem promoção'
+    acucarsuperrede.innerHTML = 'Super Rede Açucar refinado neve: 4,49R$'
 }
 function arroz(){
-    arrozmaisbarato.innerHTML = 'O arroz mais barato está no mercado Rio Sul no valor de 19,99R$'
+    arrozmaisbarato.innerHTML = 'O arroz mais barato está no mercado Super Market no valor de 19,95R$'
     arrozredeeconomia.innerHTML = 'Rede Economia Arroz branco Palmares: 22,95R$'
     arrozriosul.innerHTML = 'Rio Sul Arroz branco tucano: 19,99R$'
-    arrozsupermarket.innerHTML = 'Super Market não tem promoção'
-    arrozsuperrede.innerHTML = 'Super Rede não tem promoção'
+    arrozsupermarket.innerHTML = 'Super Market Arroz branco donal elza: 19,95R$'
+    arrozsuperrede.innerHTML = 'Super Rede Arroz branco copaarroz: 22,99R$'
 }
 function batatafrita(){
-    batatafritamaisbarato.innerHTML = 'A batata frita mais barata está no mercado Super Rede no valor de 6,99R$'
+    batatafritamaisbarato.innerHTML = 'A batata frita mais barata está no mercado Super Market no valor de 18,98R$'
     batatafritaredeeconomia.innerHTML = 'Rede Economia não tem promoção'
     batatafritariosul.innerHTML = 'Rio Sul não tem promoção'
-    batatafritasupermarket.innerHTML = 'Super Market não tem promoção'
-    batatafritasuperrede.innerHTML = 'Super Rede batata frita quality fries: 6,99R$'
+    batatafritasupermarket.innerHTML = 'Super Market batata frita ok fries ou prime fires: 18,98R$'
+    batatafritasuperrede.innerHTML = 'Super Rede batata frita golden fries: 22,95R$'
 }
 function cafe(){
     alert("Para ver adquira a versão completa")
