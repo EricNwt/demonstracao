47b6d1cd9c894f1f8dcc5e87f96995a1e1f2bfe8
skylab22 <santosericalmeida92@gmail.com>
2023-05-09 15:29:04 -0300

    Atualização

--- script.js
+++ script.js
@@ -13,54 +13,54 @@ function arroz(){
     arrozsuperrede.innerHTML = 'Super Rede Arroz não tem promoção'
 }
 function batatafrita(){
-    batatafritamaisbarato.innerHTML = 'Até o momento não tem detergente em promoção'
+    batatafritamaisbarato.innerHTML = 'Até o momento não tem batata frita em promoção'
     batatafritaredeeconomia.innerHTML = 'Rede Economia não tem promoção'
     batatafritariosul.innerHTML = 'Rio Sul não tem promoção'
     batatafritasupermarket.innerHTML = 'Super Market não tem promoção'
     batatafritasuperrede.innerHTML = 'Super Rede não tem promoção'
 }
 function cafe(){
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
 function carne(){
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
 function cerveja(){
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
 function cremedeleite(){//Talvez possa ser retirado
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
 function detergente(){//Talvez possa ser retirado
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
 function feijao(){
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
 function frango(){
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
 function leiteemcaixa(){
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
 function leiteempo(){//Talvez possa ser retirado
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
 function macarrao(){
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
 function margarina(){
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
 function molhodetomate(){//Talvez possa ser retirado
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
 function oleo(){
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
 function sabaoempo(){
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
 function rank(){
-    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 7,50 por semana ou R$ 20 por mês.")
+    alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
 }
