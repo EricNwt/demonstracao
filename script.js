f693545fdf9ed7b11fbcd2fb1690fbc93c07f278
skylab22 <santosericalmeida92@gmail.com>
2023-05-12 12:38:46 -0300

    Atual

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function acucar(){
-    acucarmaisbarato.innerHTML = 'O açucar mais barato está no mercado Rio Sul no valor de 3,99R$'
+    acucarmaisbarato.innerHTML = 'O açucar mais barato está no mercado Rio Sul/Super Market no valor de 3,99R$'
     economiaacucar.innerHTML = ''
     acucarredeeconomia.innerHTML = 'Rede Economia não tem promoção'
     acucarriosul.innerHTML = 'Rio Sul Açucar refinado caravelas: 3,99R$'
-    acucarsupermarket.innerHTML = 'Super Market não tem promoção'
+    acucarsupermarket.innerHTML = 'Super Market açucar refinado união: 3,99R$'
     acucarsuperrede.innerHTML = 'Super Rede não tem promoção'
 }
 function arroz(){
     arrozmaisbarato.innerHTML = 'O arroz mais barato está no mercado Super Market no valor de 19,95R$'
-    economiaarroz.innerHTML = 'Economizou 3,00R$ em relação ao mercado mais caro.'
-    arrozredeeconomia.innerHTML = 'Rede Economia Arroz branco Palmares: 22,95R$'
-    arrozriosul.innerHTML = 'Rio Sul Arroz branco tucano: 19,99R$'
-    arrozsupermarket.innerHTML = 'Super Market Arroz branco donal elza: 19,95R$'
-    arrozsuperrede.innerHTML = 'Super Rede Arroz não tem promoção'
+    economiaarroz.innerHTML = 'Você economizou 4,04R$ em relação ao mercado mais caro.'
+    arrozredeeconomia.innerHTML = 'Rede Economia arroz branco Palmares: 22,95R$'
+    arrozriosul.innerHTML = 'Rio Sul arroz branco tucano: 19,99R$'
+    arrozsupermarket.innerHTML = 'Super Market arroz branco donal elza: 19,95R$'
+    arrozsuperrede.innerHTML = 'Super Rede arroz careteiro: 23,99R$'
 }
 function batatafrita(){
-    batatafritamaisbarato.innerHTML = 'Até o momento não tem batata frita em promoção'
+    batatafritamaisbarato.innerHTML = 'A batata frita mais barata está no mercado Super Rede no valor de 22,95R$'
     economiabatatafrita.innerHTML = ''
     batatafritaredeeconomia.innerHTML = 'Rede Economia não tem promoção'
     batatafritariosul.innerHTML = 'Rio Sul não tem promoção'
     batatafritasupermarket.innerHTML = 'Super Market não tem promoção'
-    batatafritasuperrede.innerHTML = 'Super Rede não tem promoção'
+    batatafritasuperrede.innerHTML = 'Super Rede batata frita golden fries: 22,95R$'
 }
 function cafe(){
     alert("Para ter acesso à versão completa, clique no botão abaixo intitulado 'Versão completa' e adquira por apenas R$ 5,00 por semana ou R$ 15 por mês.")
