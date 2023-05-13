08ca4fe8f6aee253fed0886932beb4a4b29c19f8
skylab22 <santosericalmeida92@gmail.com>
2023-05-13 16:33:01 -0300

    Atual

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function acucar(){
-    acucarmaisbarato.innerHTML = 'O açucar mais barato está no mercado Rio Sul no valor de 3,99R$'
+    acucarmaisbarato.innerHTML = 'O açucar mais barato está no mercado Rio Sul/Super Market no valor de 3,99R$'
     economiaacucar.innerHTML = ''
     acucarredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
     acucarriosul.innerHTML = '<strong>Rio Sul</strong><br> Açucar refinado caravelas: 3,99R$'
-    acucarsupermarket.innerHTML = '<strong>Super Market</strong><br> não tem promoção'
+    acucarsupermarket.innerHTML = '<strong>Super Market</strong><br> Açucar refinado união: 3,99R$'
     acucarsuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
 }
 function arroz(){
@@ -12,15 +12,15 @@ function arroz(){
     arrozredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> Arroz branco Palmares: 22,95R$'
     arrozriosul.innerHTML = '<strong>Rio Sul</strong><br> Arroz branco tucano: 19,99R$'
     arrozsupermarket.innerHTML = '<strong>Super Market</strong><br> Arroz branco donal elza: 19,95R$'
-    arrozsuperrede.innerHTML = '<strong>Super Rede</strong><br> Arroz não tem promoção'
+    arrozsuperrede.innerHTML = '<strong>Super Rede</strong><br> Arroz carreteiro: 23,99R$'
 }
 function batatafrita(){
-    batatafritamaisbarato.innerHTML = 'Até o momento não tem detergente em promoção'
+    batatafritamaisbarato.innerHTML = 'A batata frita mais barata está no mercado Super Rede no valor de 22,95R$'
     economiabatatafrita.innerHTML = ''
     batatafritaredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
     batatafritariosul.innerHTML = '<strong>Rio Sul</strong><br> não tem promoção'
     batatafritasupermarket.innerHTML = '<strong>Super Market</strong><br> não tem promoção'
-    batatafritasuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
+    batatafritasuperrede.innerHTML = '<strong>Super Rede</strong><br>Batata frita golden fries: 22,95R$'
 }
 function propaganda(){
     alert("Para ter acesso à versão completa, clique no botão azul ao final da página intitulado 'Versão completa' e adquira por apenas R$ 5 por semana ou R$ 15 por mês.")
