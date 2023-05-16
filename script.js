8cf81334d771755b633541f7a5bab61f9f190c44
skylab22 <santosericalmeida92@gmail.com>
2023-05-16 12:33:55 -0300

    Atual

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 function acucar(){
-    acucarmaisbarato.innerHTML = 'Até o momento não tem promoção'
+    acucarmaisbarato.innerHTML = 'Mercado mais barato Rio Sul: 3,99R$'
     economiaacucar.innerHTML = ''
-    acucarredeeconomia.innerHTML = '<strong>Rede Economia </strong><br> não tem promoção'
-    acucarriosul.innerHTML = '<strong>Rio Sul</strong><br> não tem promoção'
-    acucarsupermarket.innerHTML = '<strong>Super Market</strong><br> não tem promoção'
-    acucarsuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
+    acucarredeeconomia.innerHTML = '<strong>Rede Economia </strong><br>Não tem promoção'
+    acucarriosul.innerHTML = '<strong>Rio Sul</strong><br>Açucar refinado caravelas: 3,99R$'
+    acucarsupermarket.innerHTML = '<strong>Super Market</strong><br>Não tem promoção'
+    acucarsuperrede.innerHTML = '<strong>Super Rede</strong><br>Não tem promoção'
 }
 function arroz(){
     arrozmaisbarato.innerHTML = 'Mercado mais barato Super Market: 19,95R$'
-    economiaarroz.innerHTML = ''
-    arrozredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
-    arrozriosul.innerHTML = '<strong>Rio Sul</strong><br> arroz branco tucano: 19,99R$'
-    arrozsupermarket.innerHTML = '<strong>Super Market</strong><br> arroz branco dona elza: 19,95R$'
+    economiaarroz.innerHTML = 'Você economizou 3,00R$ em relação ao mercado mais caro'
+    arrozredeeconomia.innerHTML = '<strong>Rede Economia</strong><br>Último preço do arroz branco Palmares: 22,95R$'
+    arrozriosul.innerHTML = '<strong>Rio Sul</strong><br>Preço atual arroz branco tucano: 19,99R$'
+    arrozsupermarket.innerHTML = '<strong>Super Market</strong><br>Preço atual arroz branco dona elza: 19,95R$'
     arrozsuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
 }
 function batatafrita(){
     batatafritamaisbarato.innerHTML = 'Até o momento não tem promoção'
     economiabatatafrita.innerHTML = ''
-    batatafritaredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
-    batatafritariosul.innerHTML = '<strong>Rio Sul</strong><br> não tem promoção'
-    batatafritasupermarket.innerHTML = '<strong>Super Market</strong><br> não tem promoção'
-    batatafritasuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
+    batatafritaredeeconomia.innerHTML = '<strong>Rede Economia</strong><br>Não tem promoção'
+    batatafritariosul.innerHTML = '<strong>Rio Sul</strong><br>Não tem promoção'
+    batatafritasupermarket.innerHTML = '<strong>Super Market</strong><br>Não tem promoção'
+    batatafritasuperrede.innerHTML = '<strong>Super Rede</strong><br>Não tem promoção'
+}
+function localizacao(){
+    localizacaolegenda.innerHTML = 'Para localizar um mercado, clique em seu nome abaixo'
+    localizacaoredeeconomia.innerHTML = '<a href="https://goo.gl/maps/9xKXonrsGNmPnmsY9" target="_blank">Rede Economia</a>'
+    localizacaoriosul.innerHTML = '<a href="https://goo.gl/maps/UHS8Tj5VBYdcQj1u5" target="_blank">Rio Sul</a>'
+    localizacaosupermarket.innerHTML = '<a href="https://goo.gl/maps/cfvPgBqujxH3XGP16" target="_blank">Super Market</a>'
+    localizacaosuperrede.innerHTML = '<a href="https://goo.gl/maps/1U3ta2AFiXdiCWkj8" target="_blank">Super Rede</a>'
 }
 function propaganda(){
     alert("Para ter acesso à versão completa, clique no botão azul ao final da página intitulado 'Versão completa' e adquira por apenas R$ 5 por semana ou R$ 15 por mês.")
